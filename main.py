31ac4e313fa2a6130186b32d3b92e738f156b56f
Soulbolt <soulbolt@gmail.com>
2023-12-13 13:59:31 -0700

    install colorgram to extract colors off an image and created a code to loop through the rgb values to store rgb values in a tuple list.

---
+++ main.py
@@ -0,0 +1,18 @@
+# import colorgram
+#
+# colors = colorgram.extract("hirst_painting.jpg", 20)
+#
+# rgb_colors = []
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+
+color_list = [(249, 250, 252), (251, 249, 241), (252, 247, 251), (244, 251, 249), (244, 228, 79), (162, 75, 42),
+              (216, 146, 90), (23, 30, 61), (124, 160, 218), (54, 89, 145), (45, 36, 30), (40, 48, 114), (29, 44, 34),
+              (147, 56, 81), (131, 31, 43), (203, 82, 120), (146, 31, 25), (214, 80, 55), (69, 31, 41), (67, 113, 77)]
